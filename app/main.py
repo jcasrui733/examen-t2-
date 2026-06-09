8accfa55972fe4af60c63eeb242281a8de4244c8
jcasrui733 <jcasrui733@fp.iesromerovargas.com>
2026-06-09 22:28:42 +0200

    comprobacion3

--- app/main.py
+++ app/main.py
@@ -11,5 +11,5 @@ app.include_router(items.router)
 # Código que se añade
 @app.get("/status")
 def version():
-    return {"status": "Castro Ruiz, Antonio - v.1.1"}
+    return {"status": "Castro Ruiz, Antonio - v.1.2"}
 
