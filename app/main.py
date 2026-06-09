c2f5ad95f2918f592809dfb10f7502c9646c840d
jcasrui733 <jcasrui733@fp.iesromerovargas.com>
2026-06-09 21:33:17 +0200

    mi primer commit

---
+++ app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from .database import Base, engine
+from .routers import items
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+app.include_router(items.router)
+
+
+# Código que se añade
+@app.get("/status")
+def version():
+    return {"status": "Castro Ruiz, Antonio - v.X.Y"}
+
